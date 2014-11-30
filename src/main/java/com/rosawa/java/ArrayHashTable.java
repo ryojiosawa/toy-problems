2b574822eee261c5a1eb566fee6bbe2deed2468d
Ryo Osawa <ryojiosawa@gmail.com>
2014-11-30 15:06:20 -0800

    Add HashTable and AllAnagrams

---
+++ src/main/java/com/rosawa/java/ArrayHashTable.java
@@ -0,0 +1,18 @@
+package com.rosawa.java;
+
+public class ArrayHashTable<K, V> implements HashTable<K, V> {
+
+    @Override
+    public void put(K key, V value) {
+
+    }
+
+    @Override
+    public V get(K key) {
+        return null;
+    }
+
+    private int hashKey(K key) {
+        return 0;
+    }
+}
